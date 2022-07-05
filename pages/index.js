2ebc470b9cb50012ed379fd06b543418bdb20ec2
Cian Butler <ciandevelops@gmail.com>
2022-07-05 14:48:42 -0400

    basic hero done

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
+import Hero from "../components/Hero";
 export default function Home() {
   return (
     <div>
@@ -10,7 +11,7 @@ export default function Home() {
       </Head>
 
       <div>
-        <h1>Hello World!</h1>
+        <Hero />
       </div>
     </div>
   );
