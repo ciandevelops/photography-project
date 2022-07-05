7c9ca59dd6ec715fd0d2d02c023155c9a396bd2a
Cian Butler <ciandevelops@gmail.com>
2022-07-05 14:39:06 -0400

    font added and clean up

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import Head from "next/head";
+import Image from "next/image";
 export default function Home() {
   return (
     <div>
@@ -13,5 +13,5 @@ export default function Home() {
         <h1>Hello World!</h1>
       </div>
     </div>
-  )
+  );
 }
