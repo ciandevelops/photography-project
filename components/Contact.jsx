478628974fa3ef80f23a42a097545571297323ae
Cian Butler <ciandevelops@gmail.com>
2022-07-06 18:22:20 -0400

    error

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -5,7 +5,7 @@ const Contact = () => {
   return (
     <div className="max-w-[1240px] m-auto p-4 h-screen">
       <h1 className="text-2xl font-bold text-center p-4">
-        Let's Work Together
+        Let&apos;s Work Together
       </h1>
       <form className="max-w-[600px] m-auto">
         <div className="grid grid-cols-2 gap-2">
